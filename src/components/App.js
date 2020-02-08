67ba60d22f161e9563d8494f1c4fc7c0a68ba385
David Wachlin <davidwachlin@gmail.com>
2020-02-08 15:06:07 -0500

    adds video iframe and select video function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends Component {
   state = {
     videos: [],
+    selectedVideo: null,
   };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -14,14 +22,34 @@ class App extends Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = video => {
+    console.log('from the app!', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <p>I have {this.state.videos.length} videos.</p>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} className="" />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/VideoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './VideoItem.css';
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  console.log(video);
+  return (
+    <div className="video-item item" onClick={() => onVideoSelect(video)}>
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
